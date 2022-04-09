491b0db24ee00b258743103259a19b42ea0eb794
Ryan Luedders <ryan.luedders@gmail.com>
2022-04-08 21:10:32 -0500

    update port for Heroku compatibility

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.get("/zipdistance", (req, res, next) => {
     });
 });
 
-app.listen(3000, () => {
- console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server running on port " + port);
 });
